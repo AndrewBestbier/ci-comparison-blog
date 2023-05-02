b459c571c96df86edfb2b488d7f4809c9fe7ce5d
Sheriff <realexcel2020@gmail.com>
2023-05-02 16:18:02 +0100

    Changed the hello world text

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World! from Sheriff'));
+app.get('/', (req, res) => res.send('Hello World! from Sheriff. The Pipeline works!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
